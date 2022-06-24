c1c5cd9e08c61f0cff4a17ac54b21b324efb3bd2
1797058516 <1797058516@qq.com>
2022-06-24 21:43:34 +0800

    Update Two_D.m

--- Two_D.m
+++ Two_D.m
@@ -8,9 +8,9 @@ T3=T1*T2;
 trplot2(T3,'frame','3','clore','g');
 T4=T2*T1;
 trplot2(T4,'frame','4','clore','c');
-P=[3;2];
+P=[3;2];%世界坐标系
 plot_point(P,'*');
-P1 = double(inv(T1)) * [P; 1];
+P1 = double(inv(T1)) * [P; 1];%以T1作为参考来描述P的坐标
 h2e(double(inv(T1))*e2h(P));
 homtrans(double(inv(T1)),P);
-P2=homtrans(double(inv(T2)),P);
+P2=homtrans(double(inv(T2)),P);%以T2作为参考来描述P的坐标
